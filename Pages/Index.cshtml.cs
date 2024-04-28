aa4689a1172d24f1382ee366861cda697d378562
Bin <vieri21bin@gmail.com>
2024-04-27 19:36:46 -0700

    add greeting in config

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,14 +6,16 @@ namespace azure_app_vieri21.Pages;
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _configuration;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger,IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public void OnGet()
     {
-
+        ViewData["Greeting"] = _configuration["Greeting"];
     }
 }
